d891a1f256a86e5fa7007813de81116e2333bad7
田頭あすか <tomorrow15526@gmail.com>
2026-01-16 11:44:34 +0900

    テストファイル追加

---
+++ src/OldVersion.java
@@ -0,0 +1,6 @@
+
+public class OldVersion {
+	 public void oldMethod() {
+	        System.out.println("古い実装です。新しい方法に移行済み。");
+	    }
+}
---
+++ src/TempTestFile.java
@@ -0,0 +1,16 @@
+
+public class TempTestFile {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		
+		 System.out.println("このファイルは削除練習用です");
+	        System.out.println("Week 6レッスン4で学習後に削除されます");
+
+	}
+	
+	public void temporaryMethod() {
+        System.out.println("一時的なメソッドです");
+    }
+
+}
